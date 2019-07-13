ce212d7d671520c579f096f81d7604c617aeebdb
tanellomondaca <tanellomondaca@users.noreply.github.com>
2019-07-13 02:45:48 -0400

    stupid

--- arriendos.php
+++ arriendos.php
@@ -400,9 +400,9 @@
                                     </div>
                                 </div>
                                 <div class="row justify-content-md-center">
-                                    <input type="submit" onclick="ingresarAbono()"
+                                    <button onclick="ingresarAbono()"
                                         class="btn btn-primary btn-user btn-block col-lg-4"
-                                        id="btn_ingresar_abono" value="Ingresar abono">
+                                        id="btn_ingresar_abono" value="Ingresar abono"></button>
                                 </div>
                             </form>
                         </div>
