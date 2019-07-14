f023eac90d1fbedb5e2047e4fb36f754d96b6013
tanellomondaca <tanellomondaca@users.noreply.github.com>
2019-07-14 00:59:46 -0400

    sd

--- arriendos.php
+++ arriendos.php
@@ -622,7 +622,7 @@
     <script src='fullcalendar/timegrid/main.js'></script>
     <script src='fullcalendar/core/locales/es.js'></script>
     <script src="js/calendario.js"></script>
-    <script src="js/arriendos.js"></script>
+ 
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
@@ -630,6 +630,8 @@
     <!-- Page level plugins -->
     <script src="vendor/datatables/jquery.dataTables.min.js"></script>
     <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <script src="js/arriendos.js"></script>
                                                 
     <!-- Page level custom scripts -->
     <script src="js/demo/datatables-demo.js"></script>
