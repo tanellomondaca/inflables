df4a077bdff681712dd86d1cf74a5d6fc56f4540
tanellomondaca <tanellomondaca@users.noreply.github.com>
2019-07-23 13:34:53 -0400

    asd

--- arriendos_excel.php
+++ arriendos_excel.php
@@ -7,19 +7,46 @@
 
     $consulta = "SELECT * FROM arriendo WHERE fecha <= '$fecha' AND fin >= '$fecha'";
     $resultado = mysqli_query($conexion,$consulta);
+
 ?>
-<table class="table table-bordered" width="100%" cellspacing="0" id="eventosDia">
+<table>
     <thead>
+        <th></th>
+        <th>Nombre</th>
+        <th>Telefono</th>
         <th>Juegos</th>
+        <th>Direccion</th>
+        <th>Comuna</th>
         <th>Inicio</th>
         <th>Fin</th>
-        <th>Dirección</th>
-        <th>Comuna</th>
+        <th>Saldo</th>
     </tr>
 <?php
     while($evento = mysqli_fetch_array( $resultado )):
 ?>
     <tr>
+        <td>
+            <?php
+                if($evento["fecha"] != $evento["fin"]){
+                    if($fecha = $evento["fecha"] ){
+                        echo "SOLO INSTALACION";
+                    }else{
+                        ECHO "SOLO RETIRO";
+                    }
+                }
+            ?>
+        </td>
+        <td>
+            <?php
+                $rut_cliente = $evento['direccion'];
+                $sql_cons = "SELECT * FROM cliente_persona WHERE rut = '$rut_cliente'";
+                $resultado_cliente = mysqli_query($conexion, $sql_cons);
+                mysqli_data_seek($resultado_cliente,0);
+                $cliente = mysqli_fetch_assoc($resultado_cliente);
+                echo $cliente["nombre"]." ".$cliente["apellido"];
+            ?>
+        </td>
+        <td><?= $evento["telefono"]; ?></td>
         <td><?php
             $cons_juegos = "SELECT * FROM juego_arriendo JA, juego J WHERE JA.id_juego = J.id AND JA.id_arriendo = '".$evento["id"]."'";
             $result_juegos = mysqli_query($conexion,$cons_juegos);
@@ -33,10 +60,12 @@
             }
         ?>
         </td>
-        <td> <?php echo substr($evento['start'],11,5); ?> </td>
-        <td> <?php echo substr($evento['end'],11,5);?> </td>
-        <td> <?php echo $evento['direccion'];?> </td>
-        <td> <?php echo $evento['comuna'];?> </td>
+        <td> <?= $evento['direccion'];?> </td>
+        <td> <?= $evento['comuna'];?> </td>
+        <td> <?= substr($evento['start'],11,5); ?> </td>
+        <td> <?= substr($evento['end'],11,5);?> </td>
+        <td><?= $evento["saldo"]; ?></td>
+        
     </tr>
 <?php endwhile; ?>
 </table>
