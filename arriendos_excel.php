9c8d331ba910f67d1452be4c3f03ce9e15832379
tanellomondaca <tanellomondaca@users.noreply.github.com>
2019-07-23 13:37:45 -0400

    generando excel

--- arriendos_excel.php
+++ arriendos_excel.php
@@ -1,6 +1,6 @@
 <?php
-    //header('Content-type: application/vnd.ms-excel;charset=iso-8859-15');
-    //header('Content-Disposition: attachment; filename=nombre_archivo.xls');
+    header('Content-type: application/vnd.ms-excel;charset=iso-8859-15');
+    header('Content-Disposition: attachment; filename=nombre_archivo.xls');
 
     include 'conexion.php';
     $fecha = $_GET['fecha'];
